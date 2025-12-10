0f806129615ab4aa14ae0041742af2f5d6e5eeaf
Radi Sarbakova <sarbakovaradina@gmail.com>
2025-12-10 22:10:55 +0200

    Запазване на локалния файл преди merge

---
+++ agents/__init__.py
@@ -0,0 +1,6 @@
+﻿from .orchestration.orchestrator import OrchestratorAgent
+from .orchestration.visualizer import VisualizerAgent
+from .orchestration.report_generator import ReportGenerator
+from .data_processing.main_agent import DataProcessingAgent
+
+__all__ = ['OrchestratorAgent', 'VisualizerAgent', 'ReportGenerator', 'DataProcessingAgent']
